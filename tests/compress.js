899ab41fbe8e15863d9247da0e9ff029dd940153
practicas-ging <practicas-ging@mail.com>
2019-04-08 11:51:56 +0200

    Updated mod10_quiz_mvc_server

--- tests/compress.js
+++ tests/compress.js
@@ -1,8 +1,8 @@
 const fs = require('fs-extra');
 const path = require('path');
 const archiver = require('archiver');
+
 const orig = path.resolve(path.join(__dirname, "mooc_node-mod10_quiz_mvc_server.zip"));
-console.log(__dirname);
 const dest = path.resolve(path.join(__dirname ,"../mooc_node-mod10_quiz_mvc_server.zip"));
 const output = fs.createWriteStream(orig);
 const archive = archiver('zip', {
